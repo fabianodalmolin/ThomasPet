c217c349f8e7cdf06ec8a198df75830d967119e3
fabianodalmolin <fabianodalmolin@gmail.com>
2026-05-06 00:18:37 -0300

    Commit 10
    Ajuste botão enviar mensagem e interação com usuário
    Ajuste botão "início" colocando as patinhas
    Revisado
    --> Falta elimitar os comentários excessivos.

--- java.js
+++ java.js
@@ -1,16 +1,46 @@
+/**
+ * Projeto: Thomas Pet
+ * Baseado nas aulas e metodologia do Professor Nicholas Macedo, inspirado projeto Blippi Pet
+ * Desenvolvido por Fabiano Dalmolin - 05/2026
+ */
+
+
+
 const formElemento = document.querySelector("form");
-const inputsElementos = document.querySelectorAll("input");
-const textoAreaElemento = document.querySelector("textarea");
+const msgSucesso = document.getElementById("mensagem-sucesso");
+const btnEnviar = formElemento.querySelector("button[type='submit']"); // Seleciona o botão
 
 formElemento.addEventListener("submit", function (event) {
-  // event.preventDefault();
+  event.preventDefault(); 
 
-  let valores = [];
+  // 1. Efeito de carregamento no início
+  const textoOriginal = btnEnviar.innerText;
+  btnEnviar.innerText = "Enviando... 🐾";
+  btnEnviar.disabled = true;
+  btnEnviar.style.opacity = "0.7";
 
-  inputsElementos.forEach(function (input) {
-    // console.log(input.value);
+  const formData = new FormData(this);
+
+  fetch(event.target.action, {
+    method: "POST",
+    body: formData,
+    headers: { 'Accept': 'application/json' }
+  })
+  .then(response => {
+    if (response.ok) {
+      msgSucesso.style.display = "block";
+      formElemento.reset(); 
+
+      setTimeout(() => {
+        msgSucesso.style.display = "none";
+      }, 5000);
+    }
+  })
+  .finally(() => {
+    // 2. Volta o botão ao normal quando termina (dando certo ou errado)
+    btnEnviar.innerText = textoOriginal;
+    btnEnviar.disabled = false;
+    btnEnviar.style.opacity = "1";
   });
-  valores.push(textoAreaElemento.value);
-  console.log("dados do formulario: ", valores);
 });
 
